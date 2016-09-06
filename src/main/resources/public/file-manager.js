039324fd3aa057321a3413b0c64d7caa99fce745
Ryan Murthick <supermarioryan@gmail.com>
2016-09-06 11:49:25 +0100

    Fallback to a simpler editor on phones

--- src/main/resources/public/file-manager.js
+++ src/main/resources/public/file-manager.js
@@ -1,3 +1,6 @@
+//include isMobile
+!function(a){var b=/iPhone/i,c=/iPod/i,d=/iPad/i,e=/(?=.*\bAndroid\b)(?=.*\bMobile\b)/i,f=/Android/i,g=/(?=.*\bAndroid\b)(?=.*\bSD4930UR\b)/i,h=/(?=.*\bAndroid\b)(?=.*\b(?:KFOT|KFTT|KFJWI|KFJWA|KFSOWI|KFTHWI|KFTHWA|KFAPWI|KFAPWA|KFARWI|KFASWI|KFSAWI|KFSAWA)\b)/i,i=/IEMobile/i,j=/(?=.*\bWindows\b)(?=.*\bARM\b)/i,k=/BlackBerry/i,l=/BB10/i,m=/Opera Mini/i,n=/(CriOS|Chrome)(?=.*\bMobile\b)/i,o=/(?=.*\bFirefox\b)(?=.*\bMobile\b)/i,p=new RegExp("(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)","i"),q=function(a,b){return a.test(b)},r=function(a){var r=a||navigator.userAgent,s=r.split("[FBAN");return"undefined"!=typeof s[1]&&(r=s[0]),s=r.split("Twitter"),"undefined"!=typeof s[1]&&(r=s[0]),this.apple={phone:q(b,r),ipod:q(c,r),tablet:!q(b,r)&&q(d,r),device:q(b,r)||q(c,r)||q(d,r)},this.amazon={phone:q(g,r),tablet:!q(g,r)&&q(h,r),device:q(g,r)||q(h,r)},this.android={phone:q(g,r)||q(e,r),tablet:!q(g,r)&&!q(e,r)&&(q(h,r)||q(f,r)),device:q(g,r)||q(h,r)||q(e,r)||q(f,r)},this.windows={phone:q(i,r),tablet:q(j,r),device:q(i,r)||q(j,r)},this.other={blackberry:q(k,r),blackberry10:q(l,r),opera:q(m,r),firefox:q(o,r),chrome:q(n,r),device:q(k,r)||q(l,r)||q(m,r)||q(o,r)||q(n,r)},this.seven_inch=q(p,r),this.any=this.apple.device||this.android.device||this.windows.device||this.other.device||this.seven_inch,this.phone=this.apple.phone||this.android.phone||this.windows.phone,this.tablet=this.apple.tablet||this.android.tablet||this.windows.tablet,"undefined"==typeof window?this:void 0},s=function(){var a=new r;return a.Class=r,a};"undefined"!=typeof module&&module.exports&&"undefined"==typeof window?module.exports=r:"undefined"!=typeof module&&module.exports&&"undefined"!=typeof window?module.exports=s():"function"==typeof define&&define.amd?define("isMobile",[],a.isMobile=s()):a.isMobile=s()}(this);
+
 var currentDir = "";
 // prepare monaco
 var editor;
@@ -9,7 +12,6 @@ var detail = $('#detailColumn');
 var editorContainer = $('#editor');
 
 var header = $('#panel-header');
-
 
 var yamlDef = {
 	// Set defaultToken to invalid to see what you do not tokenize yet
@@ -145,14 +147,37 @@ var yamlDef = {
 	},
 };
 
+var miniEditor = {
+	setValue: function (text) {
+		this.textArea.text(text);
+	},
+	getValue: function () {
+		return this.textArea.text();
+	},
+	layout: function () {
+		return null; // stub function
+	}
+};
+
 $(document).ready(function () {
     $("#save-btn").hide();
 
-	require.config({ paths: { 'vs': '/monaco-editor/dev/vs' }});
-	require(['vs/editor/editor.main'], function() {
-		monaco.languages.register({id: 'yaml'});
-		monaco.languages.setMonarchTokensProvider('yaml', yamlDef);
-	});
+	if (isMobile.any) {
+		//editorContainer.attr("contentEditable", true);
+		editor = miniEditor;
+		editor.textArea = $("<textarea id='editorArea'></textarea>");
+		editor.textArea.addClass("editorArea");
+		editorContainer.append(editor.textArea);
+
+		tabIndent.config.tab = '  ';
+		tabIndent.render(document.getElementById("editorArea"));
+	} else {
+		require.config({paths: {'vs': '/monaco-editor/dev/vs'}});
+		require(['vs/editor/editor.main'], function () {
+			monaco.languages.register({id: 'yaml'});
+			monaco.languages.setMonarchTokensProvider('yaml', yamlDef);
+		});
+	}
 
     $.ajax({
         url: "/file/",
@@ -185,7 +210,7 @@ function responsiveUi() {
 	if (singleColumn && w <= 600) return;
 	if (!singleColumn && w >= 600) return;
 
-	singleColumn = (w <= 600);
+		singleColumn = (w <= 600);
 
 	if (!singleColumn) {
 		master.show();
@@ -276,7 +301,7 @@ function openFile(file) {
         success: function (result) {
             $("#save-btn").show();
 
-			if (editor) {
+			if (editor || isMobile.any) {
 				editor.setValue(result);
 			} else {
 				editor = monaco.editor.create(document.getElementById("editor"), {
@@ -322,11 +347,6 @@ function saveFile() {
 		}
     });
 }
-
-
-
-
-
 
 // Simple JavaScript Templating
 // John Resig - http://ejohn.org/ - MIT Licensed
